7f23b7a6441200c1716a243b7e873a19ff5f795a
Adam Majer <amajer@suse.de>
2018-10-22 16:29:49 +0200

    Add ability to apply patches to staging/release only

--- patch.pl
+++ patch.pl
@@ -9,17 +9,22 @@ use warnings;
 #     Patch10: foo.patch    # PATCH_FOR: 4, 6
 #     Patch11: goo.patch
 # will have Patch10 removed in all versions not 4 and 6
+#
+# version prefix of + means staging only
+#                   = means release only
 
 my @removed_patches;
 my $cur_version=$ARGV[0];
+my $staging_matching;
 
+$staging_matching = ($cur_version =~ /staging-/) ? '\+' : '=';
 $cur_version =~ s/nodejs|staging-//;
 
 NEXT_LINE:
 while (<STDIN>) {
     my @a;
     if (@a = m/Patch([\d]+):.*PATCH_FOR:(.*)$/) {
-        if (!($a[1] =~ "\\b$cur_version\\b")) {
+        if (!($a[1] =~ /[\s,:]${staging_matching}?${cur_version}([,\s\n]|$)/)) {
             push @removed_patches, $a[0];
             next;
         }
